20e2b550c66a910a5cf9e3abd112f798f4cc1873
KuKaH <kukah99@gmail.com>
2025-11-21 00:05:49 +0900

    [feat] Network 관련 파일 추가

---
+++ BaeMinAssignment/BaeMinAssignment/week4/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  BaeMinAssignment
+//
+//  Created by 홍준범 on 11/20/25.
+//
+
+import Foundation
+
+enum Environment {
+  static let baseURL: String = "http://15.164.129.239"
+}
